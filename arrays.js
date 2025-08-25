fc6656d468b4faeec50ebf49841455c93150a2b5
md neharul islam <=rummanmail@gmail.com>
2025-08-25 13:34:06 +0200

    slette kommentare

--- arrays.js
+++ arrays.js
@@ -46,32 +46,6 @@ console.log("Opgave 4a", catBagheera);
 let catSalem = catsArray.find((cat) => cat == "Salem");
 console.log("Opgave 4b", catSalem);
 
-
-
-    // Opgave 2
-    // Tilføj katten "Sniffles" til catsArray.
-    // Udskriv arrayet i konsollen.
-    // console.log("Opgave 2", ...)
-    
-    // Opgave 3a
-    // Check om der i dogsArray er en hund som hedder "Trixie".
-    // Udskriv "true" eller "false" i konsollen afhængig af svaret.
-    // console.log("Opgave 3a", ...)
-    
-    // Opgave 3b
-    // Check om der i dogsArray er en hund som hedder "Baxter".
-    // Udskriv "true" eller "false" i konsollen afhængig af svaret.
-    // console.log("Opgave 3b", ...)
-    
-    //     Opgave 4a
-    // Check om der i catsArray er en kat som hedder "Bagheera".
-    // Udskriv katten i konsollen hvis den findes og ellers "undefined".
-    // console.log("Opgave 4a", ...)
-    
-    // Opgave 4b
-    // Check om der i catsArray er en kat som hedder "Salem".
-    // Udskriv katten i konsollen hvis den findes og ellers "undefined".
-    // console.log("Opgave 4b", ...)
     
     //     Opgave 5
     // Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle tallene i arrayet ganget med 3.
